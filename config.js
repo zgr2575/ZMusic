adba9f8e8f119623ac07573058b56a7aba7ca03b
zgr2575 <62474113+zgr2575@users.noreply.github.com>
2024-05-15 12:19:07 +0000

    del

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  challenge: false, // Set to true if you want to enable password protection.
-
-  routes: true, // Change this to false if you just want to host a bare server.
-  local: true, // Change this to false to disable local assets.
-  apiusage: false, //Setting this to true will enable the ability to access this server as an api. This will be added soon. 
-};
-export default config;
